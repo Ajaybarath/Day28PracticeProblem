8f5ebfa76d1052727700819482fa6a5260b8bd95
ajaybarath <ajaybarath3@gmail.com>
2021-09-20 19:27:47 +0530

    ADDED: main method to read csvFile and print

--- src/main/java/com/bridgeLabz/openCsvReader/OpenCsvReader.java
+++ src/main/java/com/bridgeLabz/openCsvReader/OpenCsvReader.java
@@ -1,5 +1,50 @@
 package com.bridgeLabz.openCsvReader;
 
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+
 public class OpenCsvReader {
+
+	private static final String SAMPLE_CSV_FILE_PATH = "user.csv";
+
+	public static void main(String args[]) {
+
+		try {
+			Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
+			CSVReader csvReader = new CSVReader(reader);
+
+			String[] nextRecord;
+
+			while ((nextRecord = csvReader.readNext()) != null) {
+
+				System.out.println("Name : " + nextRecord[0]);
+				System.out.println("Email : " + nextRecord[1]);
+				System.out.println("Phone : " + nextRecord[2]);
+
+			}
+			
+			
+			List<String[]> records = csvReader.readAll();
+			
+			for (String[] record: records) {
+				System.out.println("Name : " + nextRecord[0]);
+				System.out.println("Email : " + nextRecord[1]);
+				System.out.println("Phone : " + nextRecord[2]);
+
+			}
+			
+			
+			
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		
+	}
 
 }
